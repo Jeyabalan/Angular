c7655eb6b97e79b93369adb681f5c29dea22ff56
JB <giribala14@gmail.com>
2017-12-25 16:55:32 +0530

    select student info

--- angular-redux/src/app/app.component.ts
+++ angular-redux/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Student } from './app.state';
 import { Store } from '@ngrx/store';
 import { getStudentInfo } from './app.selectors';
 import { fetchStudentAction } from './actions/actions';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,10 @@ import { fetchStudentAction } from './actions/actions';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  fetchStudent: Observable<Student>;
+
   constructor(private store: Store<Student>) {
     this.store.dispatch({type: 'FETCH_STUDENT'});
+    this.fetchStudent = this.store.select(getStudentInfo);
   }
 }
