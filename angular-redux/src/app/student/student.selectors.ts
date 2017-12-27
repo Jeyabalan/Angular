2464232a8aed64b49680dd89c077870dd0bc94c3
JB <giribala14@gmail.com>
2017-12-27 20:39:25 +0530

    improvments

--- angular-redux/src/app/student/student.selectors.ts
+++ angular-redux/src/app/student/student.selectors.ts
@@ -1,7 +1,7 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { StudentState } from './student.state';
 
-export const getStudentState =  createFeatureSelector<StudentState>('students');
+export const getStudentState =  createFeatureSelector<StudentState>('student');
 
 // tslint:disable-next-line:no-shadowed-variable
 export const getUserInfo = createSelector(getStudentState, state => state.students);
